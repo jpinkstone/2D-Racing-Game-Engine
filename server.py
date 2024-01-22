38ae43d293be91a201a5e904a63111dd970dfcc9
jpinkstone <jackpinkstone1@gmail.com>
2024-01-22 15:44:47 -0500

    Added send and receive buffers

--- server.py
+++ server.py
@@ -13,7 +13,8 @@ import select
 #     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 #     return (sock.recv(10240).decode())
 
-def networking_thread(q):
+def networking_thread(send, receive):
+    print("Starting networking thread...")
     HOST = "127.0.0.1"
     PORT = 60217
 
@@ -31,7 +32,11 @@ def networking_thread(q):
             # New connection
             if notified_socket == s:
                 client_socket, client_address = s.accept()
-                message = client_socket.recv(1024).decode()
+                try:
+                    message = client_socket.recv(1024).decode()
+                except:
+                    exit_flag = True
+                    break
                 if message is False:
                     continue
                 elif message == "exit":
@@ -39,10 +44,15 @@ def networking_thread(q):
                     break
 
                 sockets_list.append(client_socket)
-                print(message)
+                receive.put(message)
             # Existing client is sending a message
             else:
-                message = notified_socket.recv(1024).decode()
+                try:
+                    message = notified_socket.recv(1024).decode()
+                except:
+                    exit_flag = True
+                    break
+
                 if message is False:
                     sockets_list.remove(notified_socket)
                     continue
@@ -50,34 +60,41 @@ def networking_thread(q):
                     exit_flag = True
                     break
 
-                print(message)
+                receive.put(message)
 
         if exit_flag:
+            print("Exiting...")
             break
 
         for notified_socket in exception_sockets:
             sockets_list.remove(notified_socket)
         
-        if q.qsize() and len(sockets_list) > 1:
+        if send.qsize() and len(sockets_list) > 1:
             for client in range(1, len(sockets_list)):
-                sockets_list[client].sendall(q.get().encode())
+                sockets_list[client].sendall(send.get().encode())
                 
     s.close()
 
-def main_thread(q):
-    string = "This is a test string to send. - server"
-    for i in range(0, 100000):
-        q.put(string + " : " + str(i))
-        # time.sleep(1)
+def main_thread(send, receive):
+    print("Starting main thread...")
+    # Send data = send.put(string)
+    # receive data = receive.get()
+    while True:
+        send.put("This is a test string to send. - server")
+        time.sleep(1)
+        if receive.qsize():
+            print(receive.get())
 
 def startup():
-    q = queue.Queue()
-    netwokring = threading.Thread(target=networking_thread, args=(q, ))
-    main = threading.Thread(target=main_thread, args=(q, ))
+    send = queue.Queue()
+    receive = queue.Queue()
+    netwokring = threading.Thread(target=networking_thread, args=(send, receive))
+    main = threading.Thread(target=main_thread, args=(send, receive))
     netwokring.start()
     main.start()
     netwokring.join()
     main.join()
+    print("Both threads have rejoined")
 
 
 if __name__ == '__main__':
