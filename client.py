c0821e61e314dbe735df9a4963d4fd69f4addd30
jpinkstone <jackpinkstone1@gmail.com>
2024-01-22 15:44:05 -0500

    Added send and receive buffers

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import socket
 import threading
 import queue
 import time
-import select
 
 # MCAST_GRP = '224.1.1.1'
 
@@ -13,7 +12,8 @@ import select
 #     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
 #     sock.sendto(str.encode(str(ip)), (MCAST_GRP, PORT))
 
-def networking_thread(q):
+def networking_thread(send, receive):
+    print("Starting networking thread...")
     HOST = "127.0.0.1"
     PORT = 60217
 
@@ -21,29 +21,38 @@ def networking_thread(q):
     s.connect((HOST, PORT))
 
     while True:
-        if q.qsize():
-            s.sendall(q.get().encode())
-        data = s.recv(1024).decode()
-        if not data or data == "exit": 
+        if send.qsize():
+            s.sendall(send.get().encode())
+        try:
+            message = s.recv(1024).decode()
+        except:
+            print("Exiting")
+            break
+        if not message or message == "exit":
+            print("Exiting")
             break
         else:
-            print(data)
+            receive.put(message)
     s.close()
 
-def main_thread(q):
-    string = "This is a test string to send. - client"
-    for i in range(0, 100000):
-        q.put(string + " : " + str(i))
-        # time.sleep(1)
+def main_thread(send, receive):
+    print("Starting main thread...")
+    while True:
+        send.put("This is a test string to send. - client")
+        time.sleep(1)
+        if receive.qsize():
+            print(receive.get())
 
 def startup():
-    q = queue.Queue()
-    netwokring = threading.Thread(target=networking_thread, args=(q, ))
-    main = threading.Thread(target=main_thread, args=(q, ))
+    send = queue.Queue()
+    receive = queue.Queue()
+    netwokring = threading.Thread(target=networking_thread, args=(send, receive))
+    main = threading.Thread(target=main_thread, args=(send, receive))
     netwokring.start()
     main.start()
     netwokring.join()
     main.join()
+    print("Both threads have rejoined")
 
 
 if __name__ == '__main__':
