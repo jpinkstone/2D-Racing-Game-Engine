b6171672c34cc9fc061aff705e213a7d09020235
Cassius Ali Garcia <82051071+cassiusgarcia1@users.noreply.github.com>
2024-01-29 10:38:55 -0500

    Updated the game engine with some basic wrappers for the game state

--- game_engine.py
+++ game_engine.py
@@ -4,6 +4,33 @@ from game_state import *
 import keyboard
 
 class GameEngine():
+    def __init__(self):
+        try:
+            self.game_state = FullGameState()
+        except:
+            print("Initilization of Game Engine Failed.")
+
+    def add_player(self):
+        player = PlayerGameState()
+        self.game_state.add_player(player)
+
+    def get_game_state(self):
+        return self.game_state
+    
+    def update_game_state(self, game_state):
+        self.game_state = game_state
+
+    def receive_game_state(self):
+        # function of whatever networking class
+        pass
+
+    def send_game_state(self):
+        # function of whatever networking class
+        pass
+
+    def get_keyboard_input(self):
+        pass
+        
     class Multiplayer():
         pass
     class Render():
