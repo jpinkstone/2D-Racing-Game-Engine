e87e891aab45b98b2a2db8913e2758abb94936af
jpinkstone <jackpinkstone1@gmail.com>
2024-01-29 11:16:25 -0500

    Added Pseudocode for structure

--- game.py
+++ game.py
@@ -1,14 +1,37 @@
 # Main game loop file that calls the game engine
-
 from game_state import *
 from game_engine import *
-from keyboard import *
 
 State = FullGameState()
 Engine = GameEngine()
 
 while (!quit):
-    NewInput = keyboard()
-    State.UpdateGameState(NewInput)
+    startTime = time()
+    # Get user input and update game state
+    UserInput = getKeyboard()
+    State.UpdateGameState(UserInput)
+
+    # Get other networking player states and update game state
+    NetworkInput = Engine.Networking.getData()
+    State.UpdateGameState(NetworkInput)
+
+    # Send current game state to other networking players
+    Engine.Networking.sendData(State.package())
+
+    # Update the HUD from the game state
+    updateHUD()
+
+    # Render the frame and do collisions and physics
     Engine.Render()
-    FPScontrol()
+
+    # Try to keep the game running at a constant FPS
+    FPScontrol(30, startTime)
+
+def getKeyboard():
+    pass
+
+def updateHUD():
+    pass
+
+def FPSControl(fps, startTime):
+    pass
