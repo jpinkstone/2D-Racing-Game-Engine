c9d7625f9bb501486315b1cb384f29542a6e14ed
jpinkstone <jackpinkstone1@gmail.com>
2024-01-31 10:59:57 -0500

    Added networking class

--- game_engine.py
+++ game_engine.py
@@ -32,20 +32,71 @@ class GameEngine():
         # function of whatever networking class
         pass
     
-    class NetworkingServer():
-        def networking_thread(send, receive):
-            print("Starting networking thread...")
-            HOST = "127.0.0.1"
-            PORT = 60217
+    class networking(self):
+        # Initialize networking variables, connection type, and data queues
+        def __init__(self, type, ip, port):
+            self.type = type
+            self.host = ip
+            self.port = port
+            self.sendQ = queue.Queue()
+            self.receiveQ = queue.Queue()
+    
+        # Start networking and main loop threads
+        def start(self, gameLoop):
+            if self.type == "server":
+                netwokring = threading.Thread(target=self.networkingThreadServer, args=())
+            else:
+                netwokring = threading.Thread(target=self.networkingThreadClient, args=())
+            main = threading.Thread(target=gameLoop, args=(self.send, self.receive))
+            netwokring.start()
+            main.start()
+            netwokring.join()
+            main.join()
+            print("Both threads have rejoined")
         
+        # Send data to all connected devices
+        def send(self, data):
+            self.sendQ.put(data)
+    
+        # Receive data from all connected devices
+        def receive(self):
+            if self.receiveQ.qsize():
+                return self.receiveQ.get()
+        
+        # Handle networking for the client devices
+        def networkingThreadClient(self):
+            print("Starting client networking thread...")
+    
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.host, self.port))
+    
+            while True:
+                if self.sendQ.qsize():
+                    s.sendall(self.sendQ.get().encode())
+                try:
+                    message = s.recv(1024).decode()
+                except:
+                    print("Exiting")
+                    break
+                if not message or message == "exit":
+                    print("Exiting")
+                    break
+                else:
+                    self.receiveQ.put(message)
+            s.close()
+        
+        # Handle networking for the server device
+        def networkingThreadServer(self):
+            print("Starting server networking thread...")
+    
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((HOST, PORT))
+            s.bind((self.host, self.port))
             s.listen()
-        
+    
             sockets_list = [s]
             exit_flag = False
-        
+    
             while True:
                 read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list, 0)
                 for notified_socket in read_sockets:
@@ -62,9 +113,9 @@ class GameEngine():
                         elif message == "exit":
                             exit_flag = True
                             break
-        
+    
                         sockets_list.append(client_socket)
-                        receive.put(message)
+                        self.receiveQ.put(message)
                     # Existing client is sending a message
                     else:
                         try:
@@ -72,100 +123,28 @@ class GameEngine():
                         except:
                             exit_flag = True
                             break
-        
+    
                         if message is False:
                             sockets_list.remove(notified_socket)
                             continue
                         elif message == "exit":
                             exit_flag = True
                             break
-        
-                        receive.put(message)
-        
+    
+                        self.receiveQ.put(message)
+    
                 if exit_flag:
                     print("Exiting...")
                     break
-        
+    
                 for notified_socket in exception_sockets:
                     sockets_list.remove(notified_socket)
                 
-                if send.qsize() and len(sockets_list) > 1:
+                if self.sendQ.qsize() and len(sockets_list) > 1:
                     for client in range(1, len(sockets_list)):
-                        sockets_list[client].sendall(send.get().encode())
+                        sockets_list[client].sendall(self.sendQ.get().encode())
                         
             s.close()
-        
-        def main_thread(send, receive):
-            print("Starting main thread...")
-            # Send data = send.put(string)
-            # receive data = receive.get()
-            while True:
-                send.put("This is a test string to send. - server")
-                time.sleep(1)
-                if receive.qsize():
-                    print(receive.get())
-        
-        def startup():
-            send = queue.Queue()
-            receive = queue.Queue()
-            netwokring = threading.Thread(target=networking_thread, args=(send, receive))
-            main = threading.Thread(target=main_thread, args=(send, receive))
-            netwokring.start()
-            main.start()
-            netwokring.join()
-            main.join()
-            print("Both threads have rejoined")
-        
-        
-        if __name__ == '__main__':
-            startup()
-    
-    class NetworkingClient():
-        def networking_thread(send, receive):
-            print("Starting networking thread...")
-            HOST = "127.0.0.1"
-            PORT = 60217
-        
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST, PORT))
-        
-            while True:
-                if send.qsize():
-                    s.sendall(send.get().encode())
-                try:
-                    message = s.recv(1024).decode()
-                except:
-                    print("Exiting")
-                    break
-                if not message or message == "exit":
-                    print("Exiting")
-                    break
-                else:
-                    receive.put(message)
-            s.close()
-        
-        def main_thread(send, receive):
-            print("Starting main thread...")
-            while True:
-                send.put("This is a test string to send. - client")
-                time.sleep(1)
-                if receive.qsize():
-                    print(receive.get())
-        
-        def startup():
-            send = queue.Queue()
-            receive = queue.Queue()
-            netwokring = threading.Thread(target=networking_thread, args=(send, receive))
-            main = threading.Thread(target=main_thread, args=(send, receive))
-            netwokring.start()
-            main.start()
-            netwokring.join()
-            main.join()
-            print("Both threads have rejoined")
-        
-        
-        if __name__ == '__main__':
-            startup()
     
     class Render():
         pass
