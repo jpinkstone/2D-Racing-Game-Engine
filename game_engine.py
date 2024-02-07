41a51ca5c16fdbaab9ef5988b6b5028823ea1818
jpinkstone <jackpinkstone1@gmail.com>
2024-02-07 12:39:59 -0500

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -2,13 +2,13 @@ import socket
 import threading
 import queue
 import select
-import time
 import pygame
-from game_state import *
+import math
 
 class GameEngine():
     def __init__(self, state):
         try:
+            self.state = state
             pygame.init()
             self.screen = pygame.display.set_mode(state.screen)
             self.screen.fill((255, 255, 255))
@@ -16,15 +16,86 @@ class GameEngine():
             self.clock = pygame.time.Clock()
         except:
             print("Initilization of Game Engine Failed.")
+
+    class EngineActions():
+        def __init__(self):
+            try:
+                self.state = self.state
+            except:
+                print("Initilization of EngineActions Failed.")
+
+        def addPlayer(self, player):
+            self.state.players.append(player)
+        
+        def startGame(self):
+            pass
+    
+    class PlayerAI():
+        def __init__(self, max_vel, rotation_vel, path=[]):
+            try:
+                self.state = self.state
+                super().__init__(max_vel, rotation_vel)
+                self.path = path
+                self.current_point = 0
+                self.vel = max_vel
+            except:
+                print("Initilization of PlayerAI Failed.")
+
+        def draw_points(self, win):
+            for point in self.path:
+                pygame.draw.circle(win, (255, 0, 0), point, 5)
+
+        def draw(self, win):
+            super().draw(win)
+            # self.draw_points(win)
+
+        def calculate_angle(self):
+            target_x, target_y = self.path[self.current_point]
+            x_diff = target_x - self.x
+            y_diff = target_y - self.y
+            if y_diff == 0:
+                desired_radian_angle = math.pi / 2
+            else:
+                desired_radian_angle = math.atan(x_diff / y_diff)
+
+            if target_y > self.y:
+                desired_radian_angle += math.pi
+
+            difference_in_angle = self.angle - math.degrees(desired_radian_angle)
+            if difference_in_angle >= 180:
+                difference_in_angle -= 360
+
+            if difference_in_angle > 0:
+                self.angle -= min(self.rotation_vel, abs(difference_in_angle))
+            else:
+                self.angle += min(self.rotation_vel, abs(difference_in_angle))
+
+        def update_path_point(self):
+            target = self.path[self.current_point]
+            rect = pygame.Rect(
+                self.x, self.y, self.img.get_width(), self.img.get_height())
+            if rect.collidepoint(*target):
+                self.current_point += 1
+        def move(self):
+            if self.current_point >= len(self.path):
+                return
+
+            self.calculate_angle()
+            self.update_path_point()
+            super().move()
     
     class networking():
         # Initialize networking variables, connection type, and data queues
         def __init__(self, type, ip, port):
-            self.type = type
-            self.host = ip
-            self.port = port
-            self.sendQ = queue.Queue()
-            self.receiveQ = queue.Queue()
+            try:
+                self.state = self.state
+                self.type = type
+                self.host = ip
+                self.port = port
+                self.sendQ = queue.Queue()
+                self.receiveQ = queue.Queue()
+            except:
+                print("Initilization of networking Failed.")
     
         # Start networking and main loop threads
         def start(self, gameLoop):
@@ -130,37 +201,68 @@ class GameEngine():
                         sockets_list[client].sendall(self.sendQ.get().encode())
                         
             s.close()
-    
-    class input():
-        def __init__(self):
+
+    class GameActions():
+        def __init__(self, action):
+            try:
+                self.state = self.state
+                if action == "accelF": self.accelF()
+                elif action == "accelB": self.accelB()
+                elif action == "decelF": self.decelF()
+                elif action == "decelB" : self.decelB()
+                elif action == "left": self.left()
+                elif action == "right": self.right()
+                elif action == "quit": self.quit()
+            except:
+                print("Initilization of GameActions Failed.")
+
+        def accelF(self):
             pass
 
-        def getInput(self):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return "quit"
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
-                        return "accelF"
-                    if event.key == pygame.K_s:
-                        return "accelB"
-                    if event.key == pygame.K_a:
-                        return "left"
-                    if event.key == pygame.K_d:
-                        return "right"
-                    if event.key == pygame.K_q:
-                        return "quit"
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
-                        return "decelF"
-                    if event.key == pygame.K_s:
-                        return "decelB"
+        def accelB(self):
+            pass
+
+        def decelF(self):
+            pass
+
+        def decelB(self):
+            pass
+
+        def left(self):
+            pass
+
+        def right(self):
+            pass
+
+        def quit(self):
+            self.state.cycle = "quit"
 
     class render():
         pygame.display.flip()
+    
+    class screen():
+        def __init__(self):
+            try:
+                self.state = self.state
+                self.screen = self.screen
+            except:
+                print("Initilization of screen Failed.")
+
+        def fill(self, color):
+            self.screen.fill(color)
+
+        def clear(self):
+            pass
+
+        def addText(self):
+            pass
+
+        def addMap(self):
+            pass
 
     class audio():
-        pass
+        def startMusic():
+            pass
 
     class physics():
         pass
