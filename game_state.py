3d924f916b00395008bb7c7900a35416ff664a00
jpinkstone <jackpinkstone1@gmail.com>
2024-02-12 16:13:05 -0500

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -12,6 +12,8 @@ class GameState:
             self.mapMask = "assets/track_mask.png"
             self.playerSprites = ["race_car_blue.png", "race_car_green.png", "race_car_pink.png", "race_car_red.png"]
             self.dimensions = (1000, 800)
+            self.gameTime = None
+            self.lastTime = None
         except:
             print("Initilization of Game State Failed.")
     
